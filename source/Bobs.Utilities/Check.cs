bedfee1022c608a8de59aa3d589ef8edd12abf80
Michael Pöttgen <michael@poettgen.de>
2015-03-11 23:27:27 +0100

    Initial project

---
+++ source/Bobs.Utilities/Check.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bobs
+{
+    public static class Check
+    {
+        public static T NotNull<T>(T value, string paramName)
+        {
+            if (value == null)
+                throw new ArgumentNullException(paramName);
+            return value;
+        }
+    }
+}
